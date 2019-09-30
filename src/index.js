423f8e8742c5ce9b8f34bef125f1bfe625b9278b
Alexandr Bet <alex.van.bet@gmail.com>
2019-09-30 23:17:33 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,58 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let obj = {};
+    let temp = '';
+    let arr =[];
+    let counter = 0;
+    let rez = '';
+    for (var key in MORSE_TABLE){
+        for(var i = 0; i < key.length; i++){
+            if (key[i] === '.'){
+                temp = temp +'10';
+            }
+            else {
+                temp = temp + '11'
+            }
+        }
+        
+        obj[temp] = MORSE_TABLE[key];
+       
+            temp = '';
+    }
+   
+    for (var key in obj){
+        arr = key.split('');
+        if (arr.length < 10){
+            counter = 10 - arr.length;
+        }
+        else {
+            continue
+        }
+       
+        for (var i = 0; i < counter; i++) {
+            arr.unshift('0');
+        }
+        obj[arr.join('')] = obj[key] ;
+        delete obj[key];
+    
+    }
+    console.log(obj);
+    for (var i = 0; i < expr.length; i++){
+        if (expr[i] == '*'){
+            rez = rez + ' ';
+            i = i + 9;
+        }
+        else{
+            for (var key in obj){ 
+                if (key == expr.substring(i, i+10)){
+                    rez = rez + obj[key];   
+                }
+            }
+            i = i + 9;
+        }
+    }
+   return(rez);  
 }
 
 module.exports = {
